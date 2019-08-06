e8c567d6ae81c1b8cba970aaa29444155f836e82
wangliang30 <wangliang30@maoyan.com>
2019-08-06 14:40:17 +0800

    提交代码

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * @Description
+ * @Author wangliang
+ * @Created 2019-08-06 14:37
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+    }
+}
