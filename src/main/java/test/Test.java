f1043077a533abb8df9769ed5085b8389cf5de0a
wangliang30 <wangliang30@maoyan.com>
2019-08-07 15:12:39 +0800

    提交简单交换代码（两种方式）

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -19,5 +19,22 @@ public class Test {
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(null);
         System.out.println(stack.isEmpty());
+
+        /**
+         * 在不借助第三个变量的情况下，如何交换两个数字？
+         */
+        int a = 2;
+        int b = 3;
+        // 通过简单的加减运算（这种方法有个问题，如果a、b非常大，两个数相加之后会超出int的范围）
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+
+        // 通过异或位运算（不用担心超出int范围的问题）
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
